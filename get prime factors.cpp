a5c061c623c7fd171a027475076e9ab74e097f7d
Akshit Lalit <76584637+Akshit1903@users.noreply.github.com>
2023-05-29 01:08:25 +0530

    Create get prime factors.cpp
    sieve method

---
+++ get prime factors.cpp
@@ -0,0 +1,24 @@
+    vector<int> v;
+    void getSieve(int n){
+        v.resize(n);
+        iota(v.begin(),v.end(),0);
+        for(int i=2;i*i<n;i++){
+            if(i!=v[i]){
+                continue;
+            }
+            for(int j=i*i;j<n;j+=i){
+                v[j]=i;
+            }
+        }
+    }
+    vector<int> getPrimes(int n){
+        vector<int> res;
+        while(n!=1){
+            int f=v[n];
+            while((n%f)==0){
+                n/=f;
+            }
+            res.push_back(f);
+        }
+        return res;
+    }
