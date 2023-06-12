6b0eb6a9cb123ac59ae2aeb795ad9f5a3b6b404c
Akshit Lalit <76584637+Akshit1903@users.noreply.github.com>
2023-06-12 13:59:39 +0530

    preorder inorder postorder

---
+++ binary tree traversals.cpp
@@ -0,0 +1,73 @@
+// RECURSIVE TOO EASY
+
+// ITERATIVE SOLUTIONS
+
+// 1. Preorder- 1 stack
+
+  vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        if(root){
+            st.push(root);
+        }
+        vector<int> preorder;
+        while(st.size()){
+            TreeNode* cur=st.top();
+            st.pop();
+            preorder.push_back(cur->val);
+            // first push right
+            if(cur->right){
+                st.push(cur->right);
+            }
+            // then push left
+            if(cur->left){
+                st.push(cur->left);
+            }
+        }
+        return preorder;
+    }
+
+// 2. Inorder- 1 stack and 1 TreeNode* variable
+
+  vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        TreeNode* node=root;
+        vector<int> inorder;
+        while(true){
+            if(node!=NULL){
+                st.push(node);
+                node=node->left;
+            }else{
+                if(st.size()==0){
+                    break;
+                }
+                TreeNode* cur=st.top();
+                st.pop();
+                inorder.push_back(cur->val);
+                node=cur->right;
+            }
+        }
+        return inorder;
+    }
+
+// 3. Postorder- 1 stacks (but reverse the array)
+
+  vector<int> postorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        if(root){
+            st.push(root);
+        }
+        vector<int> postorder;
+        while(st.size()){
+            TreeNode* cur=st.top();
+            st.pop();
+            postorder.push_back(cur->val);
+            if(cur->left){
+                st.push(cur->left);
+            }
+            if(cur->right){
+                st.push(cur->right);
+            }
+        }
+        reverse(postorder.begin(),postorder.end());
+        return postorder;
+    }
