2e34c1935e8cd5728c52f43d6d1219bce5128857
Akshit Lalit <76584637+Akshit1903@users.noreply.github.com>
2023-06-15 13:15:46 +0530

    preorder inorder postorder

--- binary tree traversals.cpp
+++ binary tree traversals.cpp
@@ -100,3 +100,95 @@
         }
         return postorder;
     }
+
+// MORRIS TRAVERSALS, O(1) Space binary tree traversals
+
+/*
+1. if(node->left is NULL), we have to explore right
+2. else go to rightest of node->left
+    I. If the current node is encountered, then we’re visiting this node a second time
+    II. else we’re visiting it for the first time, make its right connection to the current node
+
+*/
+
+// 1. Preorder traversal
+
+  vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> preorder;
+        TreeNode *node=root;
+        while(node){
+            if(node->left==NULL){
+                preorder.push_back(node->val);
+                node=node->right;
+            }else{
+                TreeNode *prev=node->left;
+                while(prev->right && prev->right!=node){
+                    prev=prev->right;
+                }
+                if(prev->right){
+                    prev->right=NULL;
+                    node=node->right;
+                }else{
+                    prev->right=node;
+                    preorder.push_back(node->val);
+                    node=node->left;
+                }
+            }
+        }
+        return preorder;
+    }
+
+// 2. Inorder Traversal
+
+  vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> inorder;
+        TreeNode *node=root;
+        while(node){
+            if(node->left == NULL){
+                inorder.push_back(node->val);
+                node=node->right;
+            }else{
+                TreeNode *it=node->left;
+                while(it->right && it->right!=node){
+                    it=it->right;
+                }
+                if(it->right){
+                    it->right=NULL;
+                    inorder.push_back(node->val);
+                    node=node->right;
+                }else{
+                    it->right=node;
+                    node=node->left;
+                }
+            }
+        }
+        return inorder;
+    }
+
+// 3. Postorder traversal
+
+  vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> postorder;
+        TreeNode *node=root;
+        while(node){
+            if(node->right==NULL){
+                postorder.push_back(node->val);
+                node=node->left;
+            }else{
+                TreeNode *it=node->right;
+                while(it->left && it->left!=node){
+                    it=it->left;
+                }
+                if(it->left){
+                    it->left=NULL;
+                    node=node->left;
+                }else{
+                    it->left=node;
+                    postorder.push_back(node->val);
+                    node=node->right;
+                }
+            }
+        }
+        reverse(postorder.begin(),postorder.end());
+        return postorder;
+    }
