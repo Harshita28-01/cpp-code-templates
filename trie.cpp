1c83bf621bb43e3affc965a82beb8174618f8647
Akshit Lalit <76584637+Akshit1903@users.noreply.github.com>
2023-06-18 00:19:16 +0530

    2 classes

--- trie.cpp
+++ trie.cpp
@@ -64,3 +64,105 @@ public:
         return true;
     }
 };
+
+// extra functions such as startsWith and erase(it deletes nodes as well)
+
+class Trie{
+    class TrieNode{
+        public:
+        TrieNode* children[26];
+        int wordCount;
+        TrieNode(){
+            wordCount=0;
+            for(int i=0;i<26;i++){
+                children[i]=NULL;
+            }
+        }
+        TrieNode *getChild(char c){
+            return children[c-'a'];
+        }
+        void createNode(char c){
+            children[c-'a']=new TrieNode();
+        }
+        void deleteChildReference(char c){
+            children[c-'a']=NULL;
+        }
+        bool allChildrenNULL(){
+            for(int i=0;i<26;i++){
+                if(children[i]){
+                    return true;
+                }
+            }
+            return false;
+        }
+    };
+    TrieNode *root;
+    int getAllWords(TrieNode *node){
+        if(node==NULL){
+            return 0;
+        }
+        int res=node->wordCount;
+        for(char c='a';c<='z';c++){
+            res+=getAllWords(node->getChild(c));
+        }
+        return res;
+    }
+    bool eraseWord(TrieNode *node,string word){
+        if(word.size()){
+            char c=word[0];
+            if(node->getChild(c)==NULL || !eraseWord(node->getChild(c),word.substr(1))){
+                return false;
+            }
+            node->deleteChildReference(c);
+        }else if(--node->wordCount){
+            return false;
+        }
+        if(node!=root && node->allChildrenNULL()){
+            delete node;
+            return true;
+        }
+        return false;
+    }
+    public:
+
+    Trie(){
+        root=new TrieNode();
+    }
+
+    void insert(string &word){
+        TrieNode *node=root;
+        for(char c:word){
+            if(node->getChild(c)==NULL){
+                node->createNode(c);
+            }
+            node=node->getChild(c);
+        }
+        node->wordCount++;
+    }
+
+    int countWordsEqualTo(string &word){
+        TrieNode *node=root;
+        for(char c:word){
+            if(node->getChild(c)==NULL){
+                return 0;
+            }
+            node=node->getChild(c);
+        }
+        return node->wordCount;
+    }
+
+    int countWordsStartingWith(string &word){
+        TrieNode *node=root;
+        for(char c:word){
+            if(node->getChild(c)==NULL){
+                return 0;
+            }
+            node=node->getChild(c);
+        }
+        return getAllWords(node);
+    }
+
+    void erase(string &word){
+        eraseWord(root,word);
+    }
+};
