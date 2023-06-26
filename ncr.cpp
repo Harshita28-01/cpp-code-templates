a36d6dbe216184f233cf2e619dd94ca4b112622e
Akshit Lalit <76584637+Akshit1903@users.noreply.github.com>
2023-06-26 13:03:11 +0530

    fast_power method, nCr=n-1Cr + n-1Cr-1 method

--- ncr.cpp
+++ ncr.cpp
@@ -1,3 +1,5 @@
+// more runtime, but useful when multiple values are required, and factorial and inverse_factorial useful
+
 using ll=long long;
 class Factorial{
     vector<int> fact;
@@ -34,3 +36,55 @@ class Factorial{
         return ((((ll)fact[n]*inv_fact[n-r])%mod)*inv_fact[r])%mod;
     }
 };
+
+
+// less runtime, optimal when only one time value required for each testcase
+
+// memoisation:-
+class Solution{
+    int dp[1001][801], mod=1e9+7;
+    int getncr(int n,int r){
+        if(n<r){
+            return 0;
+        }
+        if(n==r || r==0){
+            return 1;
+        }
+        if(dp[n][r]!=-1){
+            return dp[n][r];
+        }
+        return dp[n][r]=(getncr(n-1,r)+getncr(n-1,r-1))%mod;
+    }
+public:
+    int nCr(int n, int r){
+        memset(dp,-1,sizeof(dp));
+        return getncr(n,r);
+    }
+};
+
+// tabulation:- 
+class Solution{
+public:
+    int nCr(int n, int r){
+        if(n<r){
+            return 0;
+        }
+        int mod=1e9+7;
+        vector<vector<int>> dp(n+1,vector<int>(r+1));
+        // nC0=1
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+        // nCn=1
+        for(int i=0;i<=r;i++){
+            dp[i][i]=1;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i && j<=r;j++){
+                dp[i][j]=(dp[i-1][j]+dp[i-1][j-1])%mod;
+            }
+        }
+        return dp[n][r];
+    }
+};
+   
